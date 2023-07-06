15cf0d908e7db5c7b13665aab50d89173804bfeb
Dae <daestanii1997@gmail.com>
2023-07-06 17:45:36 -0600

    fix button and text output

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var passwordChoices = [
+    "1234567890",
+   "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+]
+
+
+let generatePassword = function() {
+    
+    let button = prompt("Are numbers required?", "Yes");
+}
